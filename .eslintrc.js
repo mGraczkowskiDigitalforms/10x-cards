308bc2de7a4d7b2fc3d1a0bad5609c72a9a38125
Mariusz Graczkowski <mariusz.graczkowski@digitalforms.pl>
2025-05-21 08:28:51 +0200

    fix: update eslint configuration to handle large files

---
+++ .eslintrc.js
@@ -0,0 +1,13 @@
+module.exports = {
+  // ... existing code ...
+  ignorePatterns: [
+    'node_modules',
+    'dist',
+    'build',
+    'coverage',
+    'test-reports',
+    '*.min.js',
+    '*.bundle.js'
+  ],
+  // ... existing code ...
+} 
